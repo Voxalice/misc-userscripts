c9ca7260e016b09b1d487ec161d012532c3fc5b5
Voxalice <64228713+Voxalice@users.noreply.github.com>
2022-04-13 18:38:19 -0500

    Update OldSongPage.user.js

--- OldSongPage.user.js
+++ OldSongPage.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         Genius - Bring Back Old Song Page
 // @namespace    http://github.com/Voxalice/
-// @version      1
+// @version      2
 // @description  Automatically opts out of new song page
 // @author       Voxalice
 // @match        https://genius.com/*
 // @icon         https://genius.com/favicon.ico
 // @grant        none
 // ==/UserScript==
-
-document.querySelector("#lyrics-root > div.LyricsControls__Container-sghmdv-0.bnFCuj > a").click();
+window.location.href.endsWith("lyrics")&&(window.location.href+="?bagon=1");
