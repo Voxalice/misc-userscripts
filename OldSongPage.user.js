2428daf6136cb9cfa46cf2932f4bc2422d23c62c
Voxalice <64228713+Voxalice@users.noreply.github.com>
2022-04-20 09:44:10 -0500

    Update OldSongPage.user.js
    Disables new song page.

--- OldSongPage.user.js
+++ OldSongPage.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         Genius - Bring Back Old Song Page
 // @namespace    https://github.com/Voxalice/
-// @version      2
-// @description  Automatically opts out of new song page
+// @version      3
+// @description  Disables new song page.
 // @author       Voxalice
 // @match        https://genius.com/*
 // @icon         https://genius.com/favicon.ico
-// @grant        none
 // ==/UserScript==
-window.location.href.endsWith("lyrics")&&(window.location.href+="?bagon=1");
+!location.search&&(location.href+="?bagon=1")
