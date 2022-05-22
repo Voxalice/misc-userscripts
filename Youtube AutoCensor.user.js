830be3769a4105570231ed92969d8f243e4840bb
Voxalice <64228713+Voxalice@users.noreply.github.com>
2022-05-22 04:41:59 -0500

    yac

---
+++ Youtube AutoCensor.user.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Youtube AutoCensor
+// @namespace    https://github.com/Voxalice
+// @version      1.0
+// @description  Automatically mutes certain words (AUTOMATIC CAPTIONS)
+// @author       Voxalice
+// @match        *://*.youtube.com/*
+// @icon         https://youtube.com/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+/*/
+    This line broadly censors harsh curse words:
+    var blockedWords = ['[ __ ]'];
+
+    May not work if YouTube tab isn't visible.
+
+    This userscript doesn't work perfectly.
+    It's based off YouTube's automatic captioning system,
+    which seems to be a lot more accurate nowadays.
+/*/
+
+// The automatic captions must end with one of the items
+// in this array to censor.
+var blockedWords = ['[ __ ]'];
+
+// Self-explanatory
+function mutePlayer(b) {
+    document.querySelector("video").muted = b;
+}
+
+setInterval(function() {
+    // Find the last caption line...
+    var ulast_caption = document.querySelectorAll(".ytp-caption-segment");
+    ulast_caption = ulast_caption[ulast_caption.length-1];
+
+    if (ulast_caption) {
+        var uls_cp_vlw = ulast_caption.innerText.split(" ");
+        uls_cp_vlw = uls_cp_vlw[uls_cp_vlw.length-1];
+
+        // Checks if automatic captions end with any array item
+        if (blockedWords.some(s => uls_cp_vlw.endsWith(s))) {
+            mutePlayer(true);
+        } else {
+            // If the mute button is toggled on, don't unmute the player
+            if (!document.querySelector("div.ytp-left-controls > span > div").ariaValueText.includes("muted")) {
+                mutePlayer(false);
+                console.log(uls_cp_vlw);
+            }
+        }
+    }
+},5);
+
+// Credz
+document.querySelector("#logo-icon").insertAdjacentText("afterend"," autocensored ");
+console.log("Youtube AutoCensor 1.0");
